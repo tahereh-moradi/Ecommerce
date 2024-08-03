fcc19808ee9f3698e9512763c1bb7c85e90c2f62
tamo <tahereh.moradi2002@gmail.com>
2024-08-03 10:29:04 +0430

    creating models

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    title = models.CharField(max_length=100)
+
+class Product(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    image = models.ImageField()
+    quantity = models.PositiveIntegerField()
+    status = models.BooleanField(default=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Cart(models.Model):
+    quantity = models.PositiveIntegerField()
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Order(models.Model):
+    total_price = models.DecimalField(decimal_places=2, max_digits=10)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    status = models.BooleanField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class OrderProduct(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(decimal_places=2, max_digits=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class PaymentLog(models.Model):
+    amount = models.DecimalField(decimal_places=2, max_digits=10)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    order_id = models.ForeignKey(Order, on_delete=models.CASCADE)
+    status = models.CharField(max_length=100)
+    error_code = models.CharField(max_length=100)
+
